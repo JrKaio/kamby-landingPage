cb7425716e292ad012e3e97a0aed4a317fcdaa50
JrKaio <kaiojunio22@hotmail.com>
2023-08-26 20:30:45 -0300

    Update

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,98 +1,86 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import Title from '../layouts/Title';
 import ContactLeft from './ContactLeft';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
-  const [username, setUsername] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
-  const [errMsg, setErrMsg] = useState("");
-  const [successMsg, setSuccessMsg] = useState("");
+  const [username, setUsername] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [successMsg, setSuccessMsg] = useState('');
 
-  // ========== Email Validation start here ==============
-  const emailValidation = () => {
-    return String(email)
-      .toLocaleLowerCase()
-      .match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/);
+  const emailValidation = (email) => {
+    const emailPattern = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/;
+    return emailPattern.test(email.toLowerCase());
   };
-  // ========== Email Validation end here ================
 
   const handleSend = (e) => {
     e.preventDefault();
-    if (username === "") {
-      setErrMsg("Username is required!");
-    } else if (phoneNumber === "") {
-      setErrMsg("Phone number is required!");
-    } else if (email === "") {
-      setErrMsg("Please give your Email!");
+    if (!username || !phoneNumber || !email || !subject || !message) {
+      setErrMsg('Todos os campos são necessários.');
     } else if (!emailValidation(email)) {
-      setErrMsg("Give a valid Email!");
-    } else if (subject === "") {
-      setErrMsg("Plese give your Subject!");
-    } else if (message === "") {
-      setErrMsg("Message is required!");
+      setErrMsg('Por favor forneça um email válido.');
     } else {
-      setSuccessMsg(
-        `Thank you dear ${username}, Your Messages has been sent Successfully!`
-      );
-      setErrMsg("");
-      setUsername("");
-      setPhoneNumber("");
-      setEmail("");
-      setSubject("");
-      setMessage("");
+      setErrMsg('');
+      emailjs
+        .send('service_dmivbkm', 'template_c8mpiyo', {
+           username,
+          email,
+           phoneNumber,
+          subject,
+          message,
+        }, 'mNennAAIA5W2UrRsB')
+        .then((response) => {
+          setSuccessMsg(`Obrigado, ${username}! Sua mensagem foi enviada com sucesso.`);
+          setUsername('');
+          setPhoneNumber('');
+          setEmail('');
+          setSubject('');
+          setMessage('');
+        })
+        .catch((error) => {
+          console.error('Error sending email:', error);
+          setErrMsg('Um erro ocorreu ao enviar esta mensagem.');
+        });
     }
   };
+
   return (
-    <section
-      id="contact"
-      className="w-full py-20 border-b-[1px] border-b-black"
-    >
+    <section id="contact" className="w-full py-20 border-b-[1px] border-b-black">
       <div className="flex justify-center items-center text-center">
-        <Title title="CONTACT" des="Contact With Me" />
+        <Title title="CONTATO" des="NOS CONTATE" />
       </div>
       <div className="w-full">
         <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
           <ContactLeft />
           <div className="w-full lgl:w-[60%] h-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] flex flex-col gap-8 p-4 lgl:p-8 rounded-lg shadow-shadowOne">
             <form className="w-full flex flex-col gap-4 lgl:gap-6 py-2 lgl:py-5">
-              {errMsg && (
-                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce">
-                  {errMsg}
-                </p>
-              )}
-              {successMsg && (
-                <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-green-500 text-base tracking-wide animate-bounce">
-                  {successMsg}
-                </p>
-              )}
               <div className="w-full flex flex-col lgl:flex-row gap-10">
                 <div className="w-full lgl:w-1/2 flex flex-col gap-4">
                   <p className="text-sm text-gray-400 uppercase tracking-wide">
-                    Your name
+                    Nome
                   </p>
                   <input
                     onChange={(e) => setUsername(e.target.value)}
                     value={username}
                     className={`${
-                      errMsg === "Username is required!" &&
-                      "outline-designColor"
+                      errMsg === 'Todos os campos são necessários.' && 'outline-designColor'
                     } contactInput`}
                     type="text"
                   />
                 </div>
                 <div className="w-full lgl:w-1/2 flex flex-col gap-4">
                   <p className="text-sm text-gray-400 uppercase tracking-wide">
-                    Phone Number
+                    Número de telefone
                   </p>
                   <input
                     onChange={(e) => setPhoneNumber(e.target.value)}
                     value={phoneNumber}
                     className={`${
-                      errMsg === "Phone number is required!" &&
-                      "outline-designColor"
+                      errMsg === 'Todos os campos são necessários.' && 'outline-designColor'
                     } contactInput`}
                     type="text"
                   />
@@ -106,35 +94,33 @@ const Contact = () => {
                   onChange={(e) => setEmail(e.target.value)}
                   value={email}
                   className={`${
-                    errMsg === "Please give your Email!" &&
-                    "outline-designColor"
+                    errMsg === 'Todos os campos são necessários.' && 'outline-designColor'
                   } contactInput`}
                   type="email"
                 />
               </div>
               <div className="flex flex-col gap-4">
                 <p className="text-sm text-gray-400 uppercase tracking-wide">
-                  Subject
+                  Assunto
                 </p>
                 <input
                   onChange={(e) => setSubject(e.target.value)}
                   value={subject}
                   className={`${
-                    errMsg === "Plese give your Subject!" &&
-                    "outline-designColor"
+                    errMsg === 'Todos os campos são necessários.' && 'outline-designColor'
                   } contactInput`}
                   type="text"
                 />
               </div>
               <div className="flex flex-col gap-4">
                 <p className="text-sm text-gray-400 uppercase tracking-wide">
-                  Message
+                  Menssagem
                 </p>
                 <textarea
                   onChange={(e) => setMessage(e.target.value)}
                   value={message}
                   className={`${
-                    errMsg === "Message is required!" && "outline-designColor"
+                    errMsg === 'Todos os campos são necessários.' && 'outline-designColor'
                   } contactTextArea`}
                   cols="30"
                   rows="8"
@@ -164,6 +150,7 @@ const Contact = () => {
       </div>
     </section>
   );
-}
+};
 
-export default Contact
+export default Contact;
+
